49fcd1172d2fd0296f87a4af0af9baeef77b539a
Sumedh Joshi <joshisumedh13@gmail.com>
2022-11-07 07:31:22 -0500

    Time: 47 ms (74.47%), Space: 13.9 MB (82.80%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,28 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        left,right=0,len(matrix[0])
+        top,bottom=0,len(matrix)
+        output=[]
+        while top < bottom and left < right:
+            #left to right
+            for i in range(left, right):
+                output.append(matrix[top][i])
+            top+=1
+            
+            for i in range(top, bottom):
+                output.append(matrix[i][right-1])
+            right-=1
+            
+            if not (left<right and top<bottom):
+                break
+            
+            for i in range(right-1, left-1, -1):
+                output.append(matrix[bottom-1][i])
+            bottom-=1
+            
+            for i in range(bottom-1, top-1,-1):
+                output.append(matrix[i][left])
+            left+=1
+        return output
+            
+        
