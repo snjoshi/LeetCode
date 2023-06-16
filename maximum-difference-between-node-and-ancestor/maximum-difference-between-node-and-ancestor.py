b6dcac06897cfae907a476137f12965dfccca1fd
Sumedh Joshi <joshisumedh13@gmail.com>
2023-06-16 07:30:28 -0400

    Time: 60 ms, Memory: 22.3 MB - LeetHub

--- maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
+++ maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
@@ -6,24 +6,25 @@
 #         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        
+
         if not root:
             return 0
-        
+
         self.result=0
-        
-        def helper(curr, curr_min, curr_max):
-            
-            if not curr:
+
+        def dfs(node, curr_min, curr_max):
+            if not node:
                 return
             
-            self.result= max(self.result,abs(curr_max-curr.val),abs(curr_min-curr.val))
-            
-            curr_min=min(curr_min,curr.val)
-            curr_max=max(curr_max,curr.val)
-            
-            helper(curr.left,curr_min,curr_max)
-            helper(curr.right,curr_min,curr_max)
+            self.result=max(self.result, abs(curr_max - node.val), abs(curr_min - node.val))
+            curr_min=min(node.val, curr_min)
+            curr_max=max(node.val, curr_max)
+
+            dfs(node.left,curr_min, curr_max)
+            dfs(node.right,curr_min, curr_max)
+
+            return self.result
         
-        helper(root, root.val,root.val)
+        dfs(root,root.val,root.val)
+
         return self.result
